077cfe2c2c39ae81c8a04169c9e308c47184077b
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-30 11:46:47 -0700

    Adds tests for match behavior while unlocked

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -17,4 +17,39 @@ class GuesserTest : public ::testing::Test {
     } // clean up after each test, (before destructor)
 };
 
+/* Tests for match behavior while unlocked */
+
+TEST(GuesserTest, match_both_empty) {
+    Guesser object("");
+    ASSERT_TRUE(object.match(""));
+}
+
+TEST(GuesserTest, match_both_one_letter_same) {
+    Guesser object("a");
+    ASSERT_TRUE(object.match("a"));
+}
+
+TEST(GuesserTest, match_both_one_letter_diff) {
+    Guesser object("a");
+    ASSERT_FALSE(object.match("b"));
+}
+
+TEST(GuesserTest, match_upper_lower_diff) {
+    Guesser object("seCret");
+    ASSERT_FALSE(object.match("secret"));
+}
+
+TEST(GuesserTest, match_upper_lower_same) {
+    Guesser object("seCret");
+    ASSERT_TRUE(object.match("seCret"));
+}
+
+TEST(GuesserTest, match_special_char_diff) {
+    Guesser object("^this is my-$ecret!");
+    ASSERT_FALSE(object.match("^this is my-secret!"));
+}
+
+TEST(GuesserTest, match_special_char_same) {
+    Guesser object("^this is my-$ecret!");
+    ASSERT_TRUE(object.match("^this is my-$ecret!"));
 }

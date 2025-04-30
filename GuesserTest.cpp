8c281ab6a49b998d162172243e049dc2f7e5b9a0
Lawjay Lee <lawjaylee1@icloud.com>
2025-04-30 11:29:33 -0700

    Autoformatter

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -1,22 +1,24 @@
 /**
  * Unit Tests for the class
-**/
+ **/
 
-#include <gtest/gtest.h>
 #include "Guesser.h"
+#include <gtest/gtest.h>
 
-class GuesserTest : public ::testing::Test
-{
-	protected:
-		GuesserTest(){} //constructor runs before each test
-		virtual ~GuesserTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+class GuesserTest : public ::testing::Test {
+  protected:
+    GuesserTest() {
+    } // constructor runs before each test
+    virtual ~GuesserTest() {
+    } // destructor cleans up after tests
+    virtual void SetUp() {
+    } // sets up before each test (after constructor)
+    virtual void TearDown() {
+    } // clean up after each test, (before destructor)
 };
 
 // Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+TEST(GuesserTest, smoke_test) {
+    Guesser object("Secret");
+    ASSERT_EQ(1 + 1, 2);
 }

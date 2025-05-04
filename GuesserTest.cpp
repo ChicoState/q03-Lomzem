63bfdd4979e6c7c65f3020fc818d06fc725039c8
Lawjay Lee <lawjaylee1@icloud.com>
2025-05-04 12:55:53 -0700

    Begin testing locked/reset behavior

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -5,6 +5,9 @@
 #include "Guesser.h"
 #include <gtest/gtest.h>
 
+/* How many guesses the Guesser object begins with */
+const unsigned int STARTING_GUESSES = 3;
+
 class GuesserTest : public ::testing::Test {
   protected:
     GuesserTest() {
@@ -52,4 +55,68 @@ TEST(GuesserTest, match_special_char_diff) {
 TEST(GuesserTest, match_special_char_same) {
     Guesser object("^this is my-$ecret!");
     ASSERT_TRUE(object.match("^this is my-$ecret!"));
+}
+
+TEST(GuesserTest, remaining_no_guess) {
+    Guesser object("Secret");
+    ASSERT_EQ(object.remaining(), STARTING_GUESSES);
+}
+
+/* Test lock behavior */
+
+TEST(GuesserTest, remaining_two_guess_close) {
+    Guesser object("Secret");
+    object.match("secret");
+    object.match("secret");
+    ASSERT_EQ(object.remaining(), STARTING_GUESSES - 2);
+}
+
+TEST(GuesserTest, match_one_close_attempt) {
+    Guesser object("Secret");
+    object.match("secret");
+    ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_out_of_attempts) {
+    Guesser object("Secret");
+    /* Assumes STARTING_GUESSES == 3 */
+    object.match("secret");
+    object.match("SecreT");
+    object.match("sEcret");
+    ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_one_last_attempt) {
+    Guesser object("Secret");
+    /* Assumes STARTING_GUESSES == 3 */
+    object.match("secret");
+    object.match("SecreT");
+    ASSERT_TRUE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_far_guess_locks) {
+    Guesser object("Secret");
+    object.match("s");
+    ASSERT_FALSE(object.match("Secret"));
+}
+
+TEST(GuesserTest, match_correct_resets_to_max) {
+    /* Tests this behavior:
+     * Whenever an unlocked secret is guessed with a true match, the guesses
+     remaining reset to three (3)
+     */
+
+    /* Assumes STARTING_GUESSES == 3 */
+
+    Guesser object("Secret");
+    object.match("secret");
+    object.match("secret");
+
+    /* Assumes unlocked at this point */
+    EXPECT_TRUE(object.match("Secret"));
+
+    /* Should have reset */
+    object.match("secret");
+    object.match("secret");
+    ASSERT_TRUE(object.match("Secret"));
 }
